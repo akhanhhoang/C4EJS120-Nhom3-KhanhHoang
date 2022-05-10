5bd4e5e6d4673949cbb161d032b8f97178e22d29
akhanhhoang <104996292+akhanhhoang@users.noreply.github.com>
2022-05-10 18:00:50 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let a = prompt("what is your firstname");
+let b = prompt("what is your lastname");
+const hello = "Hello";
+
+
+let age = prompt("how old are you");
+
+
+console.log(hello+("tôi là")+a+b+("năm nay tôi")+age+("tuổi").trim());
+
+
+
+
+
